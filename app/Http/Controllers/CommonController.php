2740d10d2cb76e511b2cc8ea3b99bb6974c44cad
Md. Minul Hasan <minulhasanrokan@gmail.com>
2023-08-29 00:09:03 +0600

    user verify

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -16,8 +16,6 @@ class CommonController extends Controller
 
     public function encrypt_data($data){
 
-        $data = urlencode($data);
-
         $encrypt_method = $this->app_session_name = config('app.app_encrypt_method');
         $secret_key = $this->app_session_name = config('app.app_encrypt_secret_key');
         $secret_iv = $this->app_session_name = config('app.app_encrypt_secret_iv');
@@ -32,12 +30,10 @@ class CommonController extends Controller
         $encryptToken = openssl_encrypt($data, $encrypt_method, $key, 0, $iv);
         $encryptToken = base64_encode($encryptToken);
 
-        return $encryptToken;
+        return urlencode($encryptToken);
     }
 
     public function decrypt_data($data){
-
-        $data = urldecode($data);
 
         $encrypt_method = $this->app_session_name = config('app.app_encrypt_method');
         $secret_key = $this->app_session_name = config('app.app_encrypt_secret_key');
@@ -52,6 +48,6 @@ class CommonController extends Controller
         // decrypt token;
         $decrypt_data = openssl_decrypt(base64_decode($data), $encrypt_method, $key, 0, $iv);
 
-        return $decrypt_data;
+        return urldecode($decrypt_data);
     }
 }
