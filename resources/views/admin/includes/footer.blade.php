30c3ce33f11d7d004a7da88a69eaf811d4824b7f
Md. Minul Hasan <minulhasanrokan@gmail.com>
2023-09-20 23:45:35 +0600

    user right

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -1,7 +1,5 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.2.0
+<footer class="main-footer" style="text-align:center;">
+    <div class="d-none d-sm-inline-block">
+        {{!empty($system_data['system_copy_right'])?$system_data['system_copy_right']:'Rental Management System'}}
     </div>
 </footer>
